4f36cab6572d821e7cbf6cd7a7e59f3a8f67bfcf
Jason Brown <iam@browniefed.com>
2014-03-11 18:41:50 -0700

    Initial commit of basic structure and auto building of the node-webkit app

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+	  nodewebkit: {
+	    options: {
+	    	version: '0.8.5',
+	        build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+	        mac: true, // We want to build it for mac
+	        win: true, // We want to build it for win
+	        linux32: false, // We don't need linux32
+	        linux64: false // We don't need linux64,
+
+	    },
+	    src: './app/**/*'// Your node-wekit app
+	  },
+	});
+
+	grunt.loadNpmTasks('grunt-node-webkit-builder');
+
+
+	grunt.registerTask( 'default', ['nodewebkit']);
+}
