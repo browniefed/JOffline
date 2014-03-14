f143f8eaff493e7cc4031688d23bf7ab7f99a72c
Jason Brown <iam@browniefed.com>
2014-03-13 21:58:01 -0700

    More shitty jQuery

--- app/js/init.js
+++ app/js/init.js
@@ -2,16 +2,21 @@ var Datastore = require('nedb')
 	, path = require('path')
   	, db = new Datastore({ filename: path.join(require('nw.gui').App.dataPath, 'joffline.db'), autoload: true });
 
-  	db.findOne({restApiUrl: {$exists: true}}, function(err, restApiDoc) {
-  		if (!err && restApiDoc) {
-  			$('.js-rest-api-url').val(restApiDoc.restApiUrl);
+  	db.findOne({restApiUrl: {$exists: true}}, function(err, userDoc) {
+  		if (!err && userDoc) {
+  			$('.js-rest-api-url').val(userDoc.restApiUrl);
+  			$('.js-username').val(userDoc.username);
+  			$('.js-password').val(userDoc.password);
   		}
   	});
 
 
   	$('.js-login-btn').on('click', function() {
-  		var val = $('.js-rest-api-url').val();
-  		db.update({ restApiUrl: {$exists: true} }, { restApiUrl: val }, { upsert: true });
+  		var val = $('.js-rest-api-url').val(),
+  			userValue = $('.js-username').val(),
+  			password = $('.js-password').val();
+
+  		db.update({ restApiUrl: {$exists: true} }, { restApiUrl: val, username: userValue, password: password }, { upsert: true });
   	});
 
 
